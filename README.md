a83dfb5c88cd1b37088eb0ba5fee3aa8b7f6f87c
Acuaro <67112172+acuaroo@users.noreply.github.com>
2023-04-21 12:58:41 -0600

    gpt2 based model

---
+++ README.md
@@ -0,0 +1,19 @@
+# GeoMagi
+
+> *LLM finetuned on data about geomagnetism*
+
+---
+
+## Data Collection
+
+`./extension/` can be loaded into the browser
+
+Users can then use the extension to record viable webpages, along with what tag the page uses to store it's primary content (e.g. span for `<span>`)
+
+Users can export their data to a `.json` file, which can then go to `./data-generation/collected/`
+
+> ![](assets/geomagi-sample.png)
+
+From there, `merge.py` merges all the data into one `scrape-list.json` *[`python merge.py`]*
+
+Finally, `scrape.js` loops through the data and produces `data.txt` *[`npm i && npm run scrape`]*
---
+++ data-generation/scrape.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import cheerio from "cheerio";
+import fs from "fs";
+
+let data = fs.readFileSync("scrape-list.json", "utf8")
+let websites = Object.values(JSON.parse(data));
+
+let scraped = [];
+let textData = [];
+
+let totalWords = 0;
+let limit = 30;
+
+function scrape(url, search) {
+    return axios(url)
+        .then(response => {
+            if (scraped.includes(url)) return;
+            scraped.push(url);
+
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const paragraphs = $(search);
+
+            paragraphs.each(function() {
+                const text = $(this).text();
+                const words = text.split(/\s+/).length;
+                
+                if (words > limit && !text.includes("...")) {
+                    if (textData.includes(text)) return;
+                    totalWords += words
+                    console.log(text);
+                    textData.push(text);
+                }
+            });
+        }).catch(console.error);
+}
+
+let scrapePromises = [];
+
+websites.forEach(files => {
+    files = Object.values(files)
+
+    files.forEach(website => {
+        scrapePromises.push(scrape(website.currentUrl, website.text));
+    });
+});
+
+Promise.all(scrapePromises).then(() => {
+    let writeData = JSON.stringify(textData, null, 1);
+    writeData = writeData.substring(1, writeData.length - 1);
+
+    writeData = writeData.replace(/\"/g, "");
+    writeData = writeData.replace(/\\t/g, "");
+    writeData = writeData.replace(/\\n/g, "");
+    writeData = writeData.replace(/(.{200}\s)/g, "$1\n")
+
+    writeData = writeData.replace(/^\s*[\r]/gm, "");
+
+    writeData = writeData.trim();
+    
+
+    console.log("estimated words (does not account for \\n or \\t): " + totalWords);
+
+    fs.writeFileSync("data.txt", writeData, "utf8");
+}).catch(console.error);
